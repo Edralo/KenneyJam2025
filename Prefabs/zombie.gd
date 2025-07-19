4eaf4c4da0a9e4e8dd3ac4c677879ee1c7984e67
Edralo <anthony.baussard@gmail.com>
2025-07-19 18:41:00 +0200

    Zombie is using walk animation

--- Prefabs/zombie.gd
+++ Prefabs/zombie.gd
@@ -6,6 +6,7 @@ func initialize(start_position, target_position):
 	look_at_from_position(start_position, target_position, Vector3.UP, true)
 	velocity = Vector3.MODEL_FRONT * speed
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
+	$"Pivot/character-l2/AnimationPlayer".play("walk")
 
 func _physics_process(_delta: float) -> void:
 	move_and_slide()
